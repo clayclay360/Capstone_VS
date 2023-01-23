c4d17f9b6403bdc0014ef890d1ea8541549f0fe1
OwenJauch13 <112638008+OwenJauch13@users.noreply.github.com>
2023-01-23 00:03:59 -0500

    Work for getting the raycast to work and finally getting the player movement put in the branch

--- CapstoneProjectVS/Assets/Scripts/Player/PlayerControlerVS.cs
+++ CapstoneProjectVS/Assets/Scripts/Player/PlayerControlerVS.cs
@@ -10,32 +10,98 @@ using UnityEngine.AI;
 
 public class PlayerControlerVS : MonoBehaviour
 {
+    //variables for movement and cameras
+    [Header("Movement")]
     private Vector3 moveVec;
     private Vector3 rotateVec;
     private float rightAnalogMagnitude;
+    public float movingSpeed;
+    public float rotatingSpeed;
+    public float movingSpeedTemp;
 
-    public float speed = 5f;
-    private Rigidbody body;
-    public float rotationSpeed;
-    void Start()
+    [Header("Camera")]
+    public Camera playerCamera;
+    private Vector3 camOffset = new Vector3(0, 10, -3);
+    private Vector3 camRotation = new Vector3(70, 0, 0);
+
+    //variables for raycast
+    public float range = 5;
+
+    void Awake()
     {
-        body = GetComponent<Rigidbody>();
-        GetComponent<Rigidbody>().mass = 0;
-        print(GetComponent<Rigidbody>().mass);
+        movingSpeedTemp = movingSpeed;
+        playerCamera.transform.position = gameObject.transform.position + camOffset;
+        playerCamera.transform.eulerAngles = camRotation;
     }
 
+    void Update()
+    {
+        Vector3 direction = Vector3.forward;
+        Ray slowSpeedRay = new Ray(transform.position + new Vector3(0f, 1.0f, 0f), transform.TransformDirection(direction * range));
+        Debug.DrawRay(transform.position + new Vector3(0f, 1.0f, 0f), transform.TransformDirection(direction * range));
+
+        if (Physics.Raycast(slowSpeedRay, out RaycastHit hit, range))
+        {
+            if (hit.collider.tag == "Obstacle")
+            {
+                //Debug.Log("Hitting Object");
+                movingSpeed = 0;
+            } else
+            {
+                movingSpeed = movingSpeedTemp;
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        PlayerMovement();
+    }
+
+    //movement for the player
     public void OnMove(InputValue value)
     {
-        var move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        move.Normalize();
+        Vector2 moveVal = value.Get<Vector2>();
+        moveVec = new Vector3(moveVal.x, 0, moveVal.y);
 
-        body.velocity = move * speed;
-
-        transform.eulerAngles = new Vector3(0, 180, 0);
+        if (rightAnalogMagnitude < 0.1f)
+        {
+            Vector2 rotateVal = value.Get<Vector2>();
+            rotateVec = new Vector3(rotateVal.x, 0, rotateVal.y);
+        }
     }
 
     public void OnLook(InputValue value)
     {
-        Debug.Log(2);
+        Vector2 rotateVal = value.Get<Vector2>();
+        rotateVec = new Vector3(rotateVal.x, 0, rotateVal.y);
+        rightAnalogMagnitude = rotateVec.magnitude;
+    }
+
+    public void PlayerMovement()
+    {
+        //Movement
+        if (Mathf.Abs(moveVec.x) > 0 || Mathf.Abs(moveVec.z) > 0)
+        {
+            if (moveVec.magnitude > .1f)
+            {
+                transform.position += movingSpeed * Time.deltaTime * moveVec;
+            }
+        }
+        //Rotation
+        if (Mathf.Abs(rotateVec.x) > 0 || Mathf.Abs(rotateVec.z) > 0)
+        {
+            Vector3 rotateDirection = (Vector3.right * rotateVec.x) + (Vector3.forward * rotateVec.z);
+            if (rotateDirection.sqrMagnitude > .1f)
+            {
+                Quaternion newRotation = Quaternion.LookRotation(rotateDirection, Vector3.up);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, newRotation, rotatingSpeed);
+                float angle = Mathf.Atan2(rotateVec.x, rotateVec.z) * Mathf.Rad2Deg;
+                transform.rotation = Quaternion.Euler(new Vector3(0, angle, 0));
+            }
+        }
+
+        //Camera
+        playerCamera.transform.position = gameObject.transform.position + camOffset;
     }
 }
