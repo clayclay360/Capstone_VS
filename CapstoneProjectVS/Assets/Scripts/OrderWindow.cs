4bde76a309dce91c9e6c2ce3210d3f53cdcddf51
KChenGitHub <chen262@canton.edu>
2023-01-23 16:27:28 -0500

    Spring Cleaning
    Removed everything I think we don't need going forward. Leave on this branch until Owen and Clay can check to make sure.

--- CapstoneProjectVS/Assets/Scripts/OrderWindow.cs
+++ CapstoneProjectVS/Assets/Scripts/OrderWindow.cs
@@ -2,58 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OrderWindow : Item
-{
-    private Plate plate;
-    private Main main;
+//Have to start the new interaction/utility interface to make the order window turn in orders again
+//public class OrderWindow : Item
+//{
+//    private Plate plate;
+//    private Main main;
 
 
-    public void Start()
-    {
+//    public void Start()
+//    {
         
-    }
+//    }
 
-    public override void CheckHand(PlayerController.ItemInMainHand item, PlayerController chef)
-    {
-        main = FindObjectOfType<Main>();
-        main.orderWindowUI.SetActive(true);
+//    public override void CheckHand(PlayerController.ItemInMainHand item, PlayerController chef)
+//    {
+//        main = FindObjectOfType<Main>();
+//        main.orderWindowUI.SetActive(true);
 
-        switch (item)
-        {
-            case PlayerController.ItemInMainHand.plate:
+//        switch (item)
+//        {
+//            case PlayerController.ItemInMainHand.plate:
 
-                if (chef.hand[0].GetComponent<Plate>() != null)
-                {
-                    plate = chef.hand[0].GetComponent<Plate>();
+//                if (chef.hand[0].GetComponent<Plate>() != null)
+//                {
+//                    plate = chef.hand[0].GetComponent<Plate>();
 
-                    if (chef.hand[0].GetComponent<Plate>().Occupied && chef.hand[0].GetComponent<Plate>().foodOnPlate.status == Status.cooked) 
-                    {
-                        Interaction = "Submit " + plate.foodOnPlate.Name;
+//                    if (chef.hand[0].GetComponent<Plate>().Occupied && chef.hand[0].GetComponent<Plate>().foodOnPlate.status == Status.cooked) 
+//                    {
+//                        Interaction = "Submit " + plate.foodOnPlate.Name;
 
-                        if (chef.isInteracting)
-                        {
-                            Debug.Log(plate.foodOnPlate.Name);
+//                        if (chef.isInteracting)
+//                        {
+//                            Debug.Log(plate.foodOnPlate.Name);
 
-                            if(plate.foodOnPlate.Name == main.mainRecipe.Name)
-                            {
-                                main.OrderComplete(plate.foodOnPlate.Name);
-                            }
-                            else if(plate.foodOnPlate.Name == main.sideRecipeOne.Name)
-                            {
-                                main.OrderComplete(plate.foodOnPlate.Name);
-                            }
-                            else if(plate.foodOnPlate.Name == main.sideRecipeTwo.Name)
-                            {
-                                main.OrderComplete(plate.foodOnPlate.Name);
-                            }
-                            chef.hand[0].Name = "";
-                        }
-                    }
-                }
-                break;
-            default:
-                Debug.Log(item);
-                break;
-        }
-    }
-}
+//                            if(plate.foodOnPlate.Name == main.mainRecipe.Name)
+//                            {
+//                                main.OrderComplete(plate.foodOnPlate.Name);
+//                            }
+//                            else if(plate.foodOnPlate.Name == main.sideRecipeOne.Name)
+//                            {
+//                                main.OrderComplete(plate.foodOnPlate.Name);
+//                            }
+//                            else if(plate.foodOnPlate.Name == main.sideRecipeTwo.Name)
+//                            {
+//                                main.OrderComplete(plate.foodOnPlate.Name);
+//                            }
+//                            chef.hand[0].Name = "";
+//                        }
+//                    }
+//                }
+//                break;
+//            default:
+//                Debug.Log(item);
+//                break;
+//        }
+//    }
+//}
--- CapstoneProjectVS/Assets/Scripts/Task.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[System.Serializable]
-public class Task
-{
-    public string taskName;
-    public bool taskCompleted;
-}
