436e331c41e8b44eb7cc03e3f7cbe8722a93dda8
KChenGitHub <chen262@canton.edu>
2023-02-05 16:58:50 -0500

    Basic Movement

--- CapstoneProjectVS/Assets/Scripts/Rat/RatController.cs
+++ CapstoneProjectVS/Assets/Scripts/Rat/RatController.cs
@@ -1,38 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
 
 public class RatController : MonoBehaviour
 {
     //Movement
     public float speed;
-    GameObject ratHoleSpawn;
-    GameObject target;
-    float distToPlayer;
+    public NavMeshAgent navAgent;
+
+    public GameObject[] targetList;
+    public GameObject currTarget;
+    public GameObject spawnHole; //where the rat spawned from
+    private float distToPlayer;
+    private bool objectiveComplete = false;
 
     //Behavior
-    enum Braveness {Timid = 3, Normal = 2, Brave = 1, Reckless = 0};
-    Braveness ratBraveness;
-    float scareDistance;
-    float scareTimer;
-    float scareTime; //couldnt remember what this variable is for, but its in the Capstone VSDD Rats document so it is here
+    public enum Braveness {Timid, Normal, Brave, Reckless};
+    public Braveness ratBraveness;
+    public float scareDistance;
+    public float scareTime; //The actual time for how long the rat is scared
+    private float scareTimer; //The incrementing timer for the rats
+    public bool isScared;
+    
 
     int health = 2;
     GameObject ratInventory;
-    GameObject possibleTargets;
-    Dictionary<GameObject, bool> currActiveTargets;
+    //Dictionary<GameObject, bool> currActiveTargets;
     GameObject closestPlayer;
 
     // Start is called before the first frame update
     void Start()
     {
-        AssignBraveness();
+        AssignTarget();
     }
 
     // Update is called once per frame
     void Update()
     {
         DistToPlayer();
+        Movement();
     }
 
     public void DistToPlayer()
@@ -41,29 +49,66 @@ public class RatController : MonoBehaviour
         distToPlayer = Vector3.Distance(closestPlayer.transform.position, transform.position);
     }
 
-    public void AssignBraveness()
+    private void AssignTarget()
     {
-        int randNum = Random.Range(0, 3);
-        if (randNum == 3)
+        //Check that our list is valid
+        if (targetList.Length == 0)
         {
-            ratBraveness = Braveness.Timid;
-            scareDistance = 5.0f;
-            scareTimer = 3.0f;
-        } else if (randNum == 2)
+            Debug.LogError("Rat target list is empty!");
+            return;
+        }
+        //Assign a target from the target list
+        int randTargetNum = Random.Range(0, targetList.Length);
+        currTarget = targetList[randTargetNum];
+        navAgent.SetDestination(currTarget.transform.position);
+        Debug.Log(currTarget);
+
+    }
+
+    /// <summary>
+    /// Logic for rat m,ovement. If the rat is scared, it runs from its source of fear.
+    /// If the rat has not completed its objective, it moves towards it.
+    /// If the rat has completed its objective, it returns to its spawn hole and despawns.
+    /// </summary>
+    private void Movement()
+    {
+        if (isScared)
         {
-            ratBraveness = Braveness.Normal;
-            scareDistance = 3.0f;
-            scareTimer = 2.0f;
-        } else if (randNum == 1)
+            navAgent.enabled = false; //We will make the rats move away from the closest player/source of fear
+            scareTimer += Time.deltaTime;
+            if (scareTimer >= scareTime)
+            {
+                isScared = false;
+                scareTimer = 0f;
+            }
+        }
+        else if (!objectiveComplete)
         {
-            ratBraveness = Braveness.Brave;
-            scareDistance = 1.5f;
-            scareTimer = 1.0f;
-        } else
+            if (Vector2.Distance(currTarget.transform.position, transform.position) <= 2.0f)
+            {
+                navAgent.isStopped = true;
+                AttackTarget();
+            }
+
+        }
+        //The rat despawns when close to the vent and has completed its objective
+        else if (Vector3.Distance(spawnHole.transform.position, transform.position) <= 1.0f)//DESPAWN
         {
-            ratBraveness = Braveness.Reckless;
-            scareDistance = 0.0f;
-            scareTimer = 0.0f;
+            Debug.Log("Destroying " + this);
+            Destroy(gameObject);
+        }
+    }
+
+    private void AttackTarget()
+    {
+        //THIS IS ALL PLACEHOLDER
+        scareTimer += Time.deltaTime;
+        if (scareTimer >= scareTime)
+        {
+            scareTimer = 0f;
+            navAgent.SetDestination(spawnHole.transform.position);
+            navAgent.isStopped = false;
+            objectiveComplete = true;
         }
     }
 }
