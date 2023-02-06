a1c31112a78e3935a2c5945d4479318404613a0c
Clay <cvaughn607@gmail.com>
2023-02-06 11:31:55 -0500

    commit 8
    - interaction
    - tools

--- CapstoneProjectVS/Assets/Scripts/Interactions/Interactable.cs
+++ CapstoneProjectVS/Assets/Scripts/Interactions/Interactable.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Interactables : MonoBehaviour
+public class Interactable : MonoBehaviour
 {
     public string Name;
     public bool canInteract;
