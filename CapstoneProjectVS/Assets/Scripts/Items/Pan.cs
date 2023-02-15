e81824502f28e375f6d07d4078ad03da4ffbbf41
Clay <cvaughn607@gmail.com>
2023-02-15 15:29:19 -0500

    commit 11
    continuing interactions

--- CapstoneProjectVS/Assets/Scripts/Items/Pan.cs
+++ CapstoneProjectVS/Assets/Scripts/Items/Pan.cs
@@ -1,15 +1,27 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Pan : Tool
 {
+    public Dictionary<int, Item> itemsInPan = new Dictionary<int, Item>();
+
     public override void Interact(Item itemInMainHand, PlayerController player)
     {
         //check to see if there's anything in the mainhand
         if(itemInMainHand != null)
         {
+            //if spatula is in main hand
             if(itemInMainHand.GetComponent<Spatula>() != null)
             {
-                //do nothing
+                //if Pan is not empty and is hot
+                if (itemsInPan.Count > 0 && isHot)
+                {
+
+                }
+                else
+                {
+                    Collect(player);
+                }
             }
             else
             {
