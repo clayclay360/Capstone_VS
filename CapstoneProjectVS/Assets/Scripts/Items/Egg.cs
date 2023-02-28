0a05aa3c6515f16d38ecabd98a796a25fe04218d
Clay <cvaughn607@gmail.com>
2023-02-27 22:21:34 -0500

    Revert "Still working on cooking"
    This reverts commit 6654d3b39cd9ded1f68e0afcda288bcfcca0bf53.

--- CapstoneProjectVS/Assets/Scripts/Items/Egg.cs
+++ CapstoneProjectVS/Assets/Scripts/Items/Egg.cs
@@ -4,16 +4,8 @@ using UnityEngine;
 
 public class Egg : Ingredients, IInteractable
 {
-    public enum State {shell, yoked, omelet, scrambled};
-    
-    [Header("State")] 
+    public enum State { shell, omelet, scrambled};
     public State state;
-
-    [Header("Models")]
-    public GameObject shellModel;
-    public GameObject omeletModel;
-    public GameObject scrambledModel;
-    public GameObject yokedModel;
 
     public override void Interact(Item itemInMainHand, PlayerController player)
     {
@@ -40,25 +32,6 @@ public class Egg : Ingredients, IInteractable
         {
             //main hand is empty
             Collect(player);
-        }
-    }
-
-    public virtual void SwitchModel(State currentState)
-    {
-        switch (currentState)
-        {
-            case State.yoked:
-                shellModel.SetActive(false);
-                yokedModel.SetActive(true);
-                break;
-            case State.omelet:
-                yokedModel.SetActive(false);
-                omeletModel.SetActive(true);
-                break;
-            case State.scrambled:
-                yokedModel.SetActive(false);
-                scrambledModel.SetActive(true);
-                break;
         }
     }
 }
