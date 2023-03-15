a2e0a34f7565b005b2e168212df3fdd7bbbf8e86
Clay <cvaughn607@gmail.com>
2023-03-15 11:31:47 -0400

    OrderManager

--- CapstoneProjectVS/Assets/Scripts/Orders/OrderManager.cs
+++ CapstoneProjectVS/Assets/Scripts/Orders/OrderManager.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem.LowLevel;
 
-public class OrderManager : MonoBehaviour
+public class OrderManager : Item
 {
-    // Update is called once per frame
-    void Update()
+    public override void Interact(Item itemInMainHand, PlayerController player)
     {
+        if(itemInMainHand != null)
+        {
+            if(itemInMainHand.GetComponent<Plate>() != null)
+            {
+                Plate plate = itemInMainHand.GetComponent<Plate>(); // get plate
+                Main main = FindObjectOfType<Main>(); // find main
+
+                // if the food on the plate is a main or side order, than the order is complete
+                if (plate.foodOnPlate[0].Name == main.mainRecipe.Name ||
+                    plate.foodOnPlate[0].Name == main.sideRecipeOne.Name ||
+                    plate.foodOnPlate[0].Name == main.sideRecipeTwo.Name)
+                {
+                    main.OrderComplete(plate.foodOnPlate[0].Name);
+                }
+            }
+        }
     }
 }
