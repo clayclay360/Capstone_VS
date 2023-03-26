f04d696ab210edbe431827d62260c584bbb96609
Clay <cvaughn607@gmail.com>
2023-03-26 15:04:50 -0400

    New Order Manager

--- CapstoneProjectVS/Assets/Scripts/Main.cs
+++ CapstoneProjectVS/Assets/Scripts/Main.cs
@@ -22,11 +22,13 @@ public class Main : MonoBehaviour
     public GameObject orderWindowUI;
 
     [HideInInspector]
-    public Recipe mainRecipe, sideRecipeOne, sideRecipeTwo;
-    private GameObject mainOrder, sideOrderOne, sideOrderTwo;
+    public Recipe mainRecipe, sideRecipeOne, sideRecipeTwo, sideRecipeThree;
+    public Recipe[] sideRecipe;
+    private GameObject mainOrder, sideOrderOne, sideOrderTwo, sideOrderThree;
+    public GameObject[] sideOrder;
 
     private float timeInBetweenOrders;
-    private int maxOrdersOfSides = 2;
+    private int maxOrdersOfSides = 3;
 
     private void Start()
     {
@@ -39,49 +41,75 @@ public class Main : MonoBehaviour
         sideRecipeOne = null;
         sideRecipeTwo = null;
         StartCoroutine(SideOrders());
-        MainOrder();
+        //MainOrder();
     }
 
-    void MainOrder()
-    {
-        mainRecipe = recipeManager[GameManager.currentLevel].mainRecipes[currentMainOrder];
-        GameObject orderGameObject = Instantiate(orderPrefab, mainOrderWindow);
-        orderGameObject.GetComponent<Order>().AssignOrder(mainRecipe.Name, 300);
-        mainOrder = orderGameObject;
-    }
+    //void MainOrder()
+    //{
+    //    mainRecipe = recipeManager[GameManager.currentLevel].mainRecipes[currentMainOrder];
+    //    GameObject orderGameObject = Instantiate(orderPrefab, mainOrderWindow);
+    //    orderGameObject.GetComponent<Order>().AssignOrder(mainRecipe.Name, 300);
+    //    mainOrder = orderGameObject;
+    //}
 
     IEnumerator SideOrders()
     {
-        while (GameManager.gameStarted)
-        {
+        //while (GameManager.gameStarted)
+        //{
             //while the game started spawn a maximum of 2 side orders
             yield return new WaitForSeconds(2);
 
-            if (maxOrdersOfSides > currentNumberOfSides)
+            for(int i = 0; i < maxOrdersOfSides; i++)
             {
-                timeInBetweenOrders = Random.Range(minTimeInBetweenOrders, maxTimeInBetweenOrders);
-                int orderIndex = Random.Range(0, recipeManager[GameManager.currentLevel].sideRecipes.Length);
+                sideRecipe[i] = recipeManager[GameManager.currentLevel].sideRecipes[i];
 
-                if(sideRecipeOne == null)
-                {
-                    sideRecipeOne = recipeManager[GameManager.currentLevel].sideRecipes[orderIndex];
-                    GameObject orderGameObject = Instantiate(orderPrefab, sideOrderWindow);
-                    orderGameObject.GetComponent<Order>().AssignOrder(sideRecipeOne.Name, 120);
-                    sideOrderOne = orderGameObject;
-                }
-                else
-                {
-                    sideRecipeTwo = recipeManager[GameManager.currentLevel].sideRecipes[orderIndex];
-                    GameObject orderGameObject = Instantiate(orderPrefab, sideOrderWindow);
-                    orderGameObject.GetComponent<Order>().AssignOrder(sideRecipeTwo.Name, 120);
-                    sideOrderTwo = orderGameObject;
-                }
+                yield return new WaitForSeconds(3);
+                GameObject orderGameObject = Instantiate(orderPrefab, sideOrderWindow);
+            orderGameObject.GetComponent<Order>().AssignOrder(sideRecipe[i].Name, 120);
 
+                sideOrder[i] = orderGameObject;
+                //if(i == 0)
+                //{
+                //    sideOrderOne = orderGameObject;
+                //}
+                //else if(i == 1)
+                //{
+                //    sideOrderTwo = orderGameObject;
+                //}
+                //else
+                //{
+                //    sideOrderThree = orderGameObject;
+                //}
                 currentNumberOfSides++;
                 FindObjectOfType<OrderManager>().DisplayIndicator(true);
-                yield return new WaitForSeconds(timeInBetweenOrders);
             }
-        }
+
+            // Old Don't Delete
+            //if (maxOrdersOfSides > currentNumberOfSides)
+            //{
+            //    timeInBetweenOrders = Random.Range(minTimeInBetweenOrders, maxTimeInBetweenOrders);
+            //    int orderIndex = Random.Range(0, recipeManager[GameManager.currentLevel].sideRecipes.Length);
+
+            //    if(sideRecipeOne == null)
+            //    {
+            //        sideRecipeOne = recipeManager[GameManager.currentLevel].sideRecipes[orderIndex];
+            //        GameObject orderGameObject = Instantiate(orderPrefab, sideOrderWindow);
+            //        orderGameObject.GetComponent<Order>().AssignOrder(sideRecipeOne.Name, 120);
+            //        sideOrderOne = orderGameObject;
+            //    }
+            //    else
+            //    {
+            //        sideRecipeTwo = recipeManager[GameManager.currentLevel].sideRecipes[orderIndex];
+            //        GameObject orderGameObject = Instantiate(orderPrefab, sideOrderWindow);
+            //        orderGameObject.GetComponent<Order>().AssignOrder(sideRecipeTwo.Name, 120);
+            //        sideOrderTwo = orderGameObject;
+            //    }
+
+            //    currentNumberOfSides++;
+            //    FindObjectOfType<OrderManager>().DisplayIndicator(true);
+            //    yield return new WaitForSeconds(timeInBetweenOrders);
+            //}
+        //}
     }
 
     public void OrderComplete(string orderName)
@@ -91,19 +119,16 @@ public class Main : MonoBehaviour
         {
             currentMainOrder++;
             Destroy(mainOrder);
-            MainOrder();
+            //MainOrder();
         }
-        else if (orderName == sideRecipeOne.Name)
+        for (int i = 0; i < sideRecipe.Length; i++)
         {
-            currentNumberOfSides--;
-            Destroy(sideOrderOne);
-            sideOrderOne = null;
-        }
-        else if(orderName == sideRecipeTwo.Name)
-        {
-            currentNumberOfSides--;
-            Destroy(sideOrderTwo);
-            sideOrderTwo = null;
+            if (sideRecipe[i].Name == orderName)
+            {
+                currentNumberOfSides--;
+                Destroy(sideOrder[i]);
+                sideOrder[i] = null;
+            }
         }
     }
 }
--- CapstoneProjectVS/Assets/Scripts/Orders/OrderManager.cs
+++ CapstoneProjectVS/Assets/Scripts/Orders/OrderManager.cs
@@ -20,24 +20,50 @@ public class OrderManager : Item
                 Main main = FindObjectOfType<Main>(); // find main
 
                 // if the food on the plate is a main or side order, then the order is complete
-                if (plate.foodOnPlate[0].Name == main.mainRecipe.Name ||
-                    plate.foodOnPlate[0].Name == main.sideRecipeTwo.Name||
-                    plate.foodOnPlate[0].Name == main.sideRecipeOne.Name)
+                for(int i = 0; main.sideRecipe.Length > i; i++)
                 {
-                    main.OrderComplete(plate.foodOnPlate[0].Name);
-                    if (plate.foodOnPlate[0].Name == "Omelet" || plate.foodOnPlate[0].Name == "Bacon" || plate.foodOnPlate[0].Name == "Toast")
+                    if (main.sideRecipe[i] != null)
                     {
-                        Ingredients ingredient;
-                        ingredient = plate.foodOnPlate[0].GetComponent<Ingredients>();
-                        plate.foodOnPlate[0].transform.parent = null;
-                        plate.foodOnPlate[0].canInteract = true;
-                        plate.foodOnPlate[0] = null;
-                        plate.isDirtied();
-                        ingredient.RespawnIngredient();
-                        ingredient.gameObject.SetActive(true);
-                        ingredient.isBeingUsed = false;
+                        if (plate.foodOnPlate[0].Name == main.sideRecipe[i].Name)
+                        {
+                            main.OrderComplete(plate.foodOnPlate[0].Name);
+                            if (plate.foodOnPlate[0].Name == "Omelet" || plate.foodOnPlate[0].Name == "Bacon" || plate.foodOnPlate[0].Name == "Toast")
+                            {
+                                Ingredients ingredient;
+                                ingredient = plate.foodOnPlate[0].GetComponent<Ingredients>();
+                                plate.foodOnPlate[0].transform.parent = null;
+                                plate.foodOnPlate[0].canInteract = true;
+                                plate.foodOnPlate.Remove(0);
+                                //plate.foodOnPlate[0] = null;
+                                plate.isDirtied();
+                                ingredient.RespawnIngredient();
+                                ingredient.gameObject.SetActive(true);
+                                ingredient.isBeingUsed = false;
+                                break;
+                            }
+                        }
                     }
                 }
+                //if (plate.foodOnPlate[0].Name == main.mainRecipe.Name ||
+                //    plate.foodOnPlate[0].Name == main.sideRecipeTwo.Name||
+                //    plate.foodOnPlate[0].Name == main.sideRecipeOne.Name ||
+                //    plate.foodOnPlate[0].Name == main.sideRecipeThree.Name)
+                //{
+                //    Debug.Log("Order Complete");
+                //    main.OrderComplete(plate.foodOnPlate[0].Name);
+                //    if (plate.foodOnPlate[0].Name == "Omelet" || plate.foodOnPlate[0].Name == "Bacon" || plate.foodOnPlate[0].Name == "Toast")
+                //    {
+                //        Ingredients ingredient;
+                //        ingredient = plate.foodOnPlate[0].GetComponent<Ingredients>();
+                //        plate.foodOnPlate[0].transform.parent = null;
+                //        plate.foodOnPlate[0].canInteract = true;
+                //        plate.foodOnPlate[0] = null;
+                //        plate.isDirtied();
+                //        ingredient.RespawnIngredient();
+                //        ingredient.gameObject.SetActive(true);
+                //        ingredient.isBeingUsed = false;
+                //    }
+                //}
             }
         }
     }
